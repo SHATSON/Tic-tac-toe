b9b098603a06304abb5e775dbcef4477399f080d
Pamba Shatson Fasco <pambakev1@gmail.com>
2021-07-06 22:24:25 +0300

    Tic Tac Toe secure app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { GameComponent } from './game/game.component';
+import { LoginComponent } from './login/login.component';
+import { AuthGuardService } from './auth-guard.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'game',
+    component: GameComponent,
+    canActivate: [ AuthGuardService ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
